478587ddfe341ff925a6f23b2763878f0e3d3edc
Brian <briantechdom@gmail.com>
2022-04-27 06:25:38 -0700

    Add example products as an array of objects

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Grid from '@material-ui/core'
 
-
+// products array as each object with it's own id
+const products = [
+    { id: 1, name: 'Shoes', description: 'Running shows.'},
+    { id: 2, name: 'Macbook', description: 'Apple Macbook.'},
+]
 const Product = () => {
     <main>
         <Grid container justify = "center" spacing={4}>
-            
+            {products.map((product) => (
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    </Grid>
+            ))}
         </Grid>
     </main>
 }
