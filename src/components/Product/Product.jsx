70c1b03b93b73fd37eebe82cc16a4b3f70fd404f
Brian <briantechdom@gmail.com>
2022-04-27 06:22:38 -0700

    Add main and Grid jsx

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,7 +3,11 @@ import Grid from '@material-ui/core'
 
 
 const Product = () => {
-
+    <main>
+        <Grid container justify = "center" spacing={4}>
+            
+        </Grid>
+    </main>
 }
 
 export default Products;
